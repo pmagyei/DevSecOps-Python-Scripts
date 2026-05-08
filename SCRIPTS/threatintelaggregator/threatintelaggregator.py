31354239bb3c9d91ea81d9f1444ff60952fcb58c
Prince Agyei <ybktalian@gmail.com>
2026-05-08 02:11:51 +0100

    added function scripts

---
+++ SCRIPTS/threatintelaggregator/threatintelaggregator.py
@@ -0,0 +1,24 @@
+# CDD
+# FUNCTION: ingest an unknown number of feeds and return a clean list
+# INPUT: unknown number of feeds, list of nested dictionaries
+# OUTPUT: combined list of single IP addresses with "high" severity, no duplicates
+
+# comments == Blueprint
+vendor_a = [{'ip': '10.0.0.5', 'sev': 'High'}, {'ip': '192.168.1.1', 'sev': 'Low'}]
+vendor_b = [{'ip': '172.16.0.8', 'sev': 'High'}, {'ip': '10.0.0.5', 'sev': 'High'}]
+vendor_c = [{'ip': '203.0.113.1', 'sev': 'Medium'}]
+
+
+# define function with positional arguments
+def aggregate_intel(*vendors: list) -> list:
+    # include typehint to explicitly state what the function expects and returns
+    ip_threats = []
+    for vendor in vendors: # iterate through each vendor using a for loop
+        for threat in vendor: # extract lists
+             if threat['sev'] == "High": # extract IP's with a "high" severity
+                 ip_threats.append(threat['ip']) # append to a new list
+    return list(set(ip_threats)) # returns list with no duplicates
+
+
+v_ip = aggregate_intel(vendor_a, vendor_b, vendor_c)
+print(v_ip)
