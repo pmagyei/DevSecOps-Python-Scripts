1bce33ec30501fad86676200bdd8b89146ec0382
Prince Agyei <ybktalian@gmail.com>
2026-06-12 15:14:56 +0100

    added api client script

---
+++ 01_capstone_projects/TI_API_Client/TI_API_Client.py
@@ -0,0 +1,29 @@
+#CDD
+# __init__ will hold url
+# __base__ to hold ip
+# url and ip can be combined into a string using f"{url}/{ip}"
+# try /except to use ConnectionError to catch failed connections, method should return {} to maintain consistency
+import json
+import requests
+
+class ThreatIntelClient:
+    def __init__(self):
+        self.target_url = None
+        self.__base_url = "https://ipapi.co/"
+
+    def scan_ip(self, target_ip: str) -> dict:
+        self.target_url = f"{self.__base_url}{target_ip}/json/"
+        try:
+            response = requests.get(self.target_url)
+            x = response.json()
+            json.dumps(x, indent=4)
+            return x
+
+        except requests.exceptions.ConnectionError:
+            print("error")
+            return {}
+
+call_api = ThreatIntelClient()
+r_ip = call_api.scan_ip("8.8.8.8")
+
+print(r_ip)
