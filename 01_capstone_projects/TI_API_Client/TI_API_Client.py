29fcbd41d5766ff508de1741618d6f8aeb4fafe2
Prince Agyei <ybktalian@gmail.com>
2026-06-12 17:55:23 +0100

    refactored TI_API script

--- 01_capstone_projects/TI_API_Client/TI_API_Client.py
+++ 01_capstone_projects/TI_API_Client/TI_API_Client.py
@@ -7,25 +7,44 @@ import json
 import requests
 from pathlib import Path
 
-output_path = Path("google.json")
+class FilePath:
+    def __init__(self):
+        pass
+    @staticmethod
+    def path_to_file(file: str):
+        output_path = Path(f"{file}")  # path to file
+        return output_path
+
 class ThreatIntelClient:
     def __init__(self):
-        self.target_url = None
         self.__base_url = "https://ipapi.co/"
 
     def scan_ip(self, target_ip: str) -> dict:
-        self.target_url = f"{self.__base_url}{target_ip}/json/"
+        target_url = f"{self.__base_url}{target_ip}/json/"
         try:
-            response = requests.get(self.target_url)
-            api_response = response.json()
-            with output_path.open("w", encoding="utf-8") as file:
-                json.dump(api_response, file, indent=4) #json.dump serializes obj as a JSON format
+            response = requests.get(target_url)  # sends a get request to the target url
+            api_response = response.json() # assigns response to variable
             return api_response
 
         except requests.exceptions.ConnectionError:
             print("error")
             return {}
 
+class SaveFile:
+    @staticmethod
+    def save_to_file(api_resip):
+        output_path = FilePath.path_to_file("google.json")
+        with output_path.open("w", encoding="utf-8") as output_file:
+            json.dump(api_resip, output_file, indent=4)  # json.dump serializes obj as a JSON format
+            return output_file
+
+path_file = FilePath.path_to_file("google.json")  #instantiate file path
 call_api = ThreatIntelClient()
-r_ip = call_api.scan_ip("8.8.8.8")
-print(r_ip)
+api_client_response = call_api.scan_ip("8.8.8.8")
+save_file = SaveFile.save_to_file(api_client_response)
+
+
+# sv = SaveFile.save_to_file("")
+# call_api = ThreatIntelClient()  # instantiate object
+# r_ip = call_api.scan_ip("8.8.8.8") # commands object to act
+# print(r_ip)
